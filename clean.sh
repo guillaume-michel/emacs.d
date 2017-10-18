cf6daaeda1edd048b8f8ec213ea2b0853ae70ff6
Guillaume MICHEL <guillaume.michel@netatmo.com>
2017-10-18 12:18:27 +0200

    - better clean up script

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -rf elpa tmp auto-save-list projectile-bookmarks.eld projectile.cache irony
+rm -rf elpa tmp auto-save-list projectile-bookmarks.eld projectile.cache irony modules/*.elc ac-comphist.dat *~ bookmarks .python-environments/ 
