608440a7be50357651306c45374f50eeb8bb49b6
Guillaume MICHEL <guillaume.michel@orilla.fr>
2018-03-01 10:44:10 +0100

    - Add toggle between horizontal and vertical split (mapped on F8)

--- modules/setup-editing.el
+++ modules/setup-editing.el
@@ -217,4 +217,33 @@ point reaches the beginning or end of the buffer, stop there."
 (use-package scala-mode
   :pin melpa)
 
+;; Vertical split shows more of each line, horizontal split shows more lines.
+;; This code toggles between them
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter
+              (if (= (car this-win-edges)
+                     (car (window-edges (next-window))))
+                  'split-window-horizontally
+                'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win-buffer)
+          (set-window-buffer (next-window) next-win-buffer)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))
+
+(global-set-key (kbd "<f8>") 'toggle-window-split)
+
 (provide 'setup-editing)
