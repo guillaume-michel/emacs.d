9251882eaafc582c3e04736931f08f29f2c3bdc3
Guillaume MICHEL <guillaume@squaremind.io>
2022-03-19 11:13:25 +0100

    Update LSP generic conf

--- modules/setup-completion.el
+++ modules/setup-completion.el
@@ -3,7 +3,7 @@
   (lsp-headerline-breadcrumb-mode))
 
 (use-package lsp-mode
-  :commands (lsp lsp-deferred)
+  ;;:commands (lsp lsp-deferred)
   :hook (lsp-mode . efs/lsp-mode-setup)
   :init
   (setq lsp-keymap-prefix "C-c l")  ;; Or 'C-l', 's-l'
@@ -24,9 +24,12 @@
   (lsp-ui-doc-header nil)
   (lsp-ui-doc-include-signature t)
   (lsp-ui-doc-alignment 'window)
-  (lsp-ui-doc-max-width 100)
+  (lsp-ui-doc-max-width 200)
   (lsp-ui-doc-max-height 13)
-  (lsp-ui-doc-delay 2))
+  (lsp-ui-doc-delay 2)
+  :config
+  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references))
 
 (use-package lsp-treemacs
   :after lsp)
