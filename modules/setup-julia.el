92cef628610ef7a7c03c545d8b0dca7fe36372e3
Guillaume MICHEL <guillaume@squaremind.io>
2022-03-19 11:13:47 +0100

    Update Julia conf

--- modules/setup-julia.el
+++ modules/setup-julia.el
@@ -4,13 +4,17 @@
 
 (use-package julia-repl
   :ensure t
-  :hook (julia-mode . julia-repl-mode))
+  :hook (julia-mode . julia-repl-mode)
+  :config
+  (set-language-environment "UTF-8")
+  (julia-repl-set-terminal-backend 'vterm)
+  (setq vterm-kill-buffer-on-exit nil))
 
 (use-package lsp-julia
   :ensure t
   :config
   (setq lsp-julia-package-dir nil
-        lsp-julia-default-environment "~/.julia/environments/v1.6"))
+        lsp-julia-default-environment "~/.julia/environments/v1.7"))
 
 (use-package flycheck-julia
   :config (add-hook 'flycheck-mode-hook #'flycheck-julia-setup))
