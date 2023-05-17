9eb7ddf01d14b909e02811ac0c4a1a841978c7e8
Guillaume MICHEL <guillaume.michel@orilla.fr>
2023-05-17 08:39:10 +0200

    Add terraform support

--- modules/setup-editing.el
+++ modules/setup-editing.el
@@ -203,4 +203,19 @@ point reaches the beginning or end of the buffer, stop there."
 (use-package meson-mode
   :defer t)
 
+(use-package terraform-mode
+  ;; if using straight
+  ;; :straight t
+
+  ;; if using package.el
+  ;; :ensure t
+  :custom (terraform-indent-level 4)
+  :config
+  (defun my-terraform-mode-init ()
+    ;; if you want to use outline-minor-mode
+    ;; (outline-minor-mode 1)
+    )
+
+  (add-hook 'terraform-mode-hook 'my-terraform-mode-init))
+
 (provide 'setup-editing)
