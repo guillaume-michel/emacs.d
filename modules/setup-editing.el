a9a68a0e5f0fc0cfb230f940d7df420cf7714f11
Guillaume MICHEL <guillaume@squaremind.io>
2024-10-22 15:48:28 +0200

    miscs config

--- modules/setup-editing.el
+++ modules/setup-editing.el
@@ -231,4 +231,30 @@ point reaches the beginning or end of the buffer, stop there."
   :hook
   (python-mode . highlight-indent-guides-mode))
 
+(use-package multiple-cursors
+  )
+
+;; Org-mode
+(use-package toc-org
+    :commands toc-org-enable
+    :init (add-hook 'org-mode-hook 'toc-org-enable))
+
+(add-hook 'org-mode-hook 'org-indent-mode)
+(use-package org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+(eval-after-load 'org-indent '(diminish 'org-indent-mode))
+
+(custom-set-faces
+ '(org-level-1 ((t (:inherit outline-1 :height 1.7))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.6))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.5))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.4))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.3))))
+ '(org-level-6 ((t (:inherit outline-5 :height 1.2))))
+ '(org-level-7 ((t (:inherit outline-5 :height 1.1)))))
+
+(require 'org-tempo)
+
+
 (provide 'setup-editing)
