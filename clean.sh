13ed8e5fe49c70862ddafaccf911a1791eb41268
Guillaume MICHEL <guillaume@squaremind.io>
2025-04-26 10:49:08 +0200

    fix clean.sh script to clean native compiled code

--- clean.sh
+++ clean.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-rm -rf elpa tmp auto-save-list projectile-bookmarks.eld projectile.cache irony modules/*.elc ac-comphist.dat *~ bookmarks .python-environments/ 
-
+rm -rf elpa tmp auto-save-list projectile-bookmarks.eld projectile.cache irony modules/*.elc ac-comphist.dat *~ bookmarks .python-environments/ eln-cache 
 # cleanup straight
 rm -rf straight/build-cache.el straight/build
