f0654a7b53e3f443b99608e8e973e4afb4d3c21a
Guillaume MICHEL <guillaume@squaremind.io>
2025-04-26 10:49:29 +0200

    fix yasnippet path

--- modules/setup-completion.el
+++ modules/setup-completion.el
@@ -112,7 +112,7 @@
   :init (add-hook 'after-init-hook 'yas-global-mode)
   :config
   (setq yas-snippet-dirs
-        '("~/.emacs.d/snippets"))
+        '("~/.config/emacs/snippets"))
   (define-key yas-minor-mode-map (kbd "<tab>") nil)
   (define-key yas-minor-mode-map (kbd "TAB") nil)
   (define-key yas-minor-mode-map (kbd "SPC") yas-maybe-expand)
